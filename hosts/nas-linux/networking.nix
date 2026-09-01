40a17ed1466f69b001f96dfddcefd6bfb6b37233
knight <2579671619@qq.com>
2026-09-01 21:18:58 +0800

    进一步写死代理

--- hosts/nas-linux/networking.nix
+++ hosts/nas-linux/networking.nix
@@ -19,13 +19,6 @@
     allowedUDPPorts = [45123];
   };
 
-  networking.proxy = {
-    default = "http://127.0.0.1:7890";
-    httpProxy = "http://127.0.0.1:7890";
-    httpsProxy = "http://127.0.0.1:7890";
-    noProxy = "localhost,127.0.0.1,::1,*.local";
-  };
-
   boot.kernel.sysctl = {
     "net.ipv6.conf.all.use_tempaddr" = lib.mkForce 0;
     "net.ipv6.conf.default.use_tempaddr" = lib.mkForce 0;
