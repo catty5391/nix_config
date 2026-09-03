225b7361e5b44a3d3bda6f6e00cfe7e29d65c9b3
knight <2579671619@qq.com>
2026-09-03 11:51:10 +0800

    添加yazi配置

--- home/knight/programs/development/yazi.nix
+++ home/knight/programs/development/yazi.nix
@@ -6,5 +6,21 @@
       package = pkgs.yaziPlugins.git;
       setup = true;
     };
+    settings = {
+      plugin = {
+        prepend_fetchers = [
+          {
+            id = "git";
+            name = "*";
+            run = "git";
+          }
+          {
+            id = "git";
+            name = "*/";
+            run = "git";
+          }
+        ];
+      };
+    };
   };
 }
